ce40af81c5cfb54ca851a898ae570b408a7e96ad
mwlaofr <millenaofrr@gmail.com>
2025-12-16 09:59:36 -0300

    fix: api url

--- User-Interface/services/api.ts
+++ User-Interface/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://10.109.3.169:8080/api",
+  baseURL: "http://169.254.188.118:8080/api",
 });
 
 export default api;
--- User-Interface/services/apiCatalog.ts
+++ User-Interface/services/apiCatalog.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const apiCatalog = axios.create({
-  baseURL: "http://10.109.3.169:8080/catalog",
+  baseURL: "http://169.254.188.118:8080/catalog",
 });
